4e528176d858c95f225d0f85fb313cc4284d8015
buddydip <buddydip@gmail.com>
2021-01-07 21:26:10 +0530

    Add files via upload

--- SensorIntegration/Arduino/AirQualitySensor/AirQualitySensor.ino
+++ SensorIntegration/Arduino/AirQualitySensor/AirQualitySensor.ino
@@ -1,9 +1,9 @@
-  #include <MQ2.h>
+#include <MQ2.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 #include <SoftwareSerial.h>
-SoftwareSerial s(5,6); // (Rx, Tx)
+SoftwareSerial s(5, 6); // (Rx, Tx)
 
 //JSON message for switch control
 StaticJsonDocument<200> doc;
@@ -13,6 +13,8 @@ StaticJsonDocument<200> doc;
 #define MQ2_PIN A0
 #define PIR_PIN 12
 #define DHTTYPE DHT11   // DHT 11 
+#define SOUND_PIN A1
+
 
 DHT dht = DHT(DHTPIN, DHTTYPE);
 float temperature = 0;
@@ -20,6 +22,9 @@ float humidity = 0;
 float oldtemperature = 0;
 float oldhumidity = 0;
 float heatIndex = 0;
+float sound = 0;
+float oldSound = 0;
+
 
 //MQ135
 int lpg, co, smoke;
@@ -31,22 +36,21 @@ MQ2 mq2(MQ2_PIN);
 boolean motion = false;
 long lastMotionTime = 0;
 String lastMotionTimeformattted = "";
-//long motionDetectionInterval = 300000;
 long motionDetectionInterval = 60000;
-long delayTime = 60000;
+long delayTime = 30000;
 
 void setup() {
 
   Serial.begin(115200);
   s.begin(115200);
   Serial.flush();
-  
+
   pinMode(PIR_PIN, INPUT);
   pinMode(MQ2_PIN, INPUT);
 
   dht.begin();
   mq2.begin();
-  
+
   delay(2000);
 }
 
@@ -56,11 +60,10 @@ void loop() {
   oldtemperature = temperature;
   oldhumidity = humidity;
 
-   // Read the humidity in %:
+  // Read the humidity in %:
   humidity = dht.readHumidity();
   // Read the temperature as Celsius:
   temperature = dht.readTemperature();
-
 
   // Check if any reads failed and exit early (to try again):
   if (isnan(humidity) || isnan(temperature)) {
@@ -71,12 +74,13 @@ void loop() {
 
   // Compute heat index in Celsius:
   heatIndex = dht.computeHeatIndex(temperature, humidity, false);
-  
+
   //MQ2 Gas Sensor Reading
-  float* values= mq2.read(true); //set it false if you don't want to print the values in the Serial
+  float* values = mq2.read(true); //set it false if you don't want to print the values in the Serial
   oldlpg = lpg;
-  oldco=co;
-  oldsmoke=smoke;
+  oldco = co;
+  oldsmoke = smoke;
+  oldSound = sound;
 
   //lpg = values[0];
   lpg = mq2.readLPG();
@@ -84,6 +88,9 @@ void loop() {
   co = mq2.readCO();
   //smoke = values[2];
   smoke = mq2.readSmoke();
+
+  sound = analogRead(SOUND_PIN);
+
 
   Serial.print("ESP Serial Available: ");
   Serial.println(s.available());
@@ -97,43 +104,36 @@ void loop() {
   Serial.print("Heat Index : ");
   Serial.println(heatIndex);
 
-  Serial.print("LPG : ");
-  Serial.println(values[0]);
-
-  Serial.print("CO2 : ");
-  Serial.println(values[1]);
+  Serial.print("Sound : ");
+  Serial.println(sound);
   
-  Serial.print("Smoke : ");
-  Serial.println(values[2]);
-
-    
-    if (digitalRead(PIR_PIN) == HIGH)
-    {        
-        motion = true;
-        long nowTime = millis();
-        Serial.println("Motion Detected!");
-        if ((nowTime - lastMotionTime) > motionDetectionInterval)
-        {
-          outputString = "";
-          doc["ParamName"] = "Motion";
-          doc["ParamValue"] = motion;
-          serializeJson(doc, outputString);
-          Serial.println(outputString);
-          lastMotionTime = nowTime;
-
-          //write to serial for NodeMCU to read
-          s.print(outputString);
-          s.println();
-          doc.clear();
-          delay(1000);          
-        }
-    }
-    else
+  if (digitalRead(PIR_PIN) == HIGH)
+  {
+    motion = true;
+    long nowTime = millis();
+    Serial.println("Motion Detected!");
+    if ((nowTime - lastMotionTime) > motionDetectionInterval)
     {
-      motion = false;
+      outputString = "";
+      doc["ParamName"] = "Motion";
+      doc["ParamValue"] = motion;
+      serializeJson(doc, outputString);
+      Serial.println(outputString);
+      lastMotionTime = nowTime;
+
+      //write to serial for NodeMCU to read
+      s.print(outputString);
+      s.println();
+      doc.clear();
+      delay(1000);
     }
- 
-  if ((abs(oldtemperature - temperature) > 1) && !isnan(temperature))
+  }
+  else
+  {
+    motion = false;
+  }
+
+  if ((oldtemperature != temperature)  && !isnan(temperature))
   {
     outputString = "";
     doc["ParamName"] = "Temperature";
@@ -141,24 +141,7 @@ void loop() {
     serializeJson(doc, outputString);
 
     Serial.println(outputString);
-    
-    //write to serial for NodeMCU to read
-    s.print(outputString);
-    s.println();
-    doc.clear();
-    delay(1000);    
-  }
 
-
-  if ((abs(oldhumidity - humidity) > 1) && !isnan(humidity))
-  {
-    outputString = "";
-    doc["ParamName"] = "Humidity";
-    doc["ParamValue"] = humidity;
-    serializeJson(doc, outputString);
-
-    Serial.println(outputString);
-    
     //write to serial for NodeMCU to read
     s.print(outputString);
     s.println();
@@ -166,7 +149,24 @@ void loop() {
     delay(1000);
   }
 
-  if (((abs(oldtemperature - temperature) > 1) || (abs(oldhumidity - humidity) > 1)) && !isnan(heatIndex))
+
+  if ((oldhumidity != humidity) && !isnan(humidity))
+  {
+    outputString = "";
+    doc["ParamName"] = "Humidity";
+    doc["ParamValue"] = humidity;
+    serializeJson(doc, outputString);
+
+    Serial.println(outputString);
+
+    //write to serial for NodeMCU to read
+    s.print(outputString);
+    s.println();
+    doc.clear();
+    delay(1000);
+  }
+
+  if (((oldtemperature != temperature) || (oldhumidity != humidity) ) && !isnan(heatIndex))
   {
     outputString = "";
     doc["ParamName"] = "HeatIndex";
@@ -174,7 +174,7 @@ void loop() {
     serializeJson(doc, outputString);
 
     Serial.println(outputString);
-    
+
     //write to serial for NodeMCU to read
     s.print(outputString);
     s.println();
@@ -190,7 +190,7 @@ void loop() {
     serializeJson(doc, outputString);
 
     Serial.println(outputString);
-    
+
     //write to serial for NodeMCU to read
     s.print(outputString);
     s.println();
@@ -206,7 +206,7 @@ void loop() {
     serializeJson(doc, outputString);
 
     Serial.println(outputString);
-    
+
     //write to serial for NodeMCU to read
     s.print(outputString);
     s.println();
@@ -222,12 +222,28 @@ void loop() {
     serializeJson(doc, outputString);
 
     Serial.println(outputString);
-    
+
     //write to serial for NodeMCU to read
     s.print(outputString);
     s.println();
     doc.clear();
   }
+
+  if (oldSound != sound)
+  {
+    outputString = "";
+    doc["ParamName"] = "Sound";
+    doc["ParamValue"] = sound;
+    serializeJson(doc, outputString);
+
+    Serial.println(outputString);
+
+    //write to serial for NodeMCU to read
+    s.print(outputString);
+    s.println();
+    doc.clear();
+  }
+
 
   delay(delayTime);
 }
